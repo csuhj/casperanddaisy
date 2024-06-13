2870c19ece8e52f46f28191b1418c5384b46df80
csuhj <csuhj@users.noreply.github.com>
2024-06-13 19:58:16 +0100

    Added routing and a placeholder fixtures page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./containers/home/home.component').then(mod => mod.HomeComponent)},
+    { path: 'fixtures', loadComponent: () => import('./containers/fixtures/fixtures.component').then(mod => mod.FixturesComponent)},
+];
---
+++ src/app/containers/fixtures/fixtures.component.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Fixture } from '../../models/fixture/fixture';
+import { Venue } from '../../models/venue/venue';
+import { FixtureService } from '../../services/fixture/fixture.service';
+import { VenueService } from '../../services/venue/venue.service';
+import { Subject, combineLatest, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'app-fixtures',
+  standalone: true,
+  imports: [],
+  templateUrl: './fixtures.component.html',
+  styleUrl: './fixtures.component.scss'
+})
+export class FixturesComponent {
+  public fixtures?: Fixture[];
+  public venues?: Venue[];
+
+  private readonly destroy$ = new Subject<void>();
+
+  public constructor(private changeDetectorRef: ChangeDetectorRef, private fixtureService: FixtureService, private venueService: VenueService) {}
+
+  ngOnInit(): void {
+    combineLatest([
+      this.fixtureService.getFixtures(),
+      this.venueService.getVenues(),
+    ])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(([fixtures, venues]) => {
+        this.fixtures = fixtures;
+        this.venues = venues;
+
+        this.changeDetectorRef.detectChanges();
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
