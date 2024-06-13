78a58349eb4d639038bb53aca592201c2ff51947
csuhj <csuhj@users.noreply.github.com>
2024-06-13 21:09:01 +0100

    update fixtures to include group predictions

--- src/app/containers/fixtures/fixtures.component.ts
+++ src/app/containers/fixtures/fixtures.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
-import { Fixture } from '../../models/fixture/fixture';
+import { Fixture, RoundEnum } from '../../models/fixture/fixture';
 import { Venue } from '../../models/venue/venue';
 import { FixtureService } from '../../services/fixture/fixture.service';
 import { VenueService } from '../../services/venue/venue.service';
 import { Subject, combineLatest, takeUntil } from 'rxjs';
 import { DatePipe } from '@angular/common';
+import { PredictionService } from '../../services/prediction/prediction.service';
+import { Result } from '../../models/result/result';
+import { TeamService } from '../../services/team/team.service';
 
 @Component({
   selector: 'app-fixtures',
@@ -16,20 +19,32 @@ import { DatePipe } from '@angular/common';
 export class FixturesComponent {
   public fixtures?: Fixture[];
   public venues?: Venue[];
+  public predictions?: Result[];
 
   private readonly destroy$ = new Subject<void>();
 
-  public constructor(private changeDetectorRef: ChangeDetectorRef, private fixtureService: FixtureService, private venueService: VenueService) {}
+  public constructor(private changeDetectorRef: ChangeDetectorRef, private fixtureService: FixtureService, private venueService: VenueService, private teamService: TeamService, private predictionService: PredictionService) {}
 
   ngOnInit(): void {
     combineLatest([
       this.fixtureService.getFixtures(),
       this.venueService.getVenues(),
+      this.teamService.getTeams(),
     ])
       .pipe(takeUntil(this.destroy$))
-      .subscribe(([fixtures, venues]) => {
+      .subscribe(([fixtures, venues, teams]) => {
         this.fixtures = fixtures;
         this.venues = venues;
+
+        this.predictions = this.fixtures
+          ?.filter(f => f.round === RoundEnum.Group)
+          .map(f => {
+            const homeTeam = teams.find(t => t.name === f.home);
+            const awayTeam = teams.find(t => t.name === f.away);
+            return (!homeTeam || !awayTeam)? 
+              new Result({}) : 
+              this.predictionService.getPrediction(homeTeam, awayTeam);
+          });
 
         this.changeDetectorRef.detectChanges();
       });
@@ -38,5 +53,11 @@ export class FixturesComponent {
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  public getGroupPrediction(fixture: Fixture) {
+    return fixture.round !== RoundEnum.Group? 
+      undefined :
+      this.predictions?.find(p => p.home === fixture.home && p.away === fixture.away);
   }
 }
